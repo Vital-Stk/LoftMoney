24010daae2e4da963e8a5391acae4d1ceaf6f55c
Vital-Stk <vtl.stk@gmail.com>
2019-06-17 22:05:52 +0200

    initial commit LoftMoney

---
+++ app/src/main/java/com/loftschool/vtlstk/loftmoney/MainActivity.java
@@ -0,0 +1,14 @@
+package com.loftschool.vtlstk.loftmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
